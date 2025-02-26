4d8c14636bb5eefa88faa6fc127a319943e3383a
bibek190 <bibek9818917036@gmail.com>
2025-02-27 02:18:05 +1100

    redo react

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,17 @@
-import './style.css'
-import { createRoot } from 'react-dom/client'
+import "./style.css";
+import { createRoot } from "react-dom/client";
 
-const root = createRoot(document.querySelector('#root'))
+const root = createRoot(document.querySelector("#root"));
 
 root.render(
-    <h1>Hello React</h1>
-)
+  <>
+    <h1 className="main-title">I am a Web developer</h1>
+    <h2>Hey React dev developer</h2>
+    <h3>Bigboss is here</h3>
+    <button>
+      <span>C</span>lick
+    </button>
+    <input type="checkbox" id="the-checkbox"></input>
+    <label htmlFor="the-checkbox">That checkbox</label>
+  </>
+);
